93d395a79ab31699276ded86c0f3461ad283d2b2
ceeden <emilcedendahl@gmail.com>
2026-07-09 18:27:11 +0200

    mod version bump

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -16,5 +16,5 @@ object BuildConfig {
     const val SODIUM_VERSION: String = "0.9.0+mc26.2"
 
     //BBE mod version (remember to bump!!!)
-    const val MOD_VERSION: String = "1.3.6"
+    const val MOD_VERSION: String = "1.3.7"
 }
