77bca7de6064a8ebdadcc86fd06633a8254676ec
ceeden <emilcedendahl@gmail.com>
2026-07-09 17:13:56 +0200

    change shade mode option default to vanilla

--- fabric/src/main/java/betterblockentities/client/gui/config/builder/ConfigBuilder.java
+++ fabric/src/main/java/betterblockentities/client/gui/config/builder/ConfigBuilder.java
@@ -18,7 +18,7 @@ public class ConfigBuilder {
 
         int defaultBannerGraphics = EnumTypes.BannerGraphicsType.FANCY.ordinal();
         int defaultUpdateScheduler = EnumTypes.UpdateSchedulerType.FAST.ordinal();
-        int defaultShadeMode = EnumTypes.ShadeMode.SODIUM.ordinal();
+        int defaultShadeMode = EnumTypes.ShadeMode.VANILLA.ordinal();
 
         List<OptionObject<?>> options = new ArrayList<>(List.of(
                 new BooleanOption("optimize.master", true),
